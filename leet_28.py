2fa09b0ecf818f334fb4163ebd3c4c85627fa8b1
Hrishikesh Keraba Lohar <117097875+hrishikeshlohar2901@users.noreply.github.com>
2023-11-22 22:46:36 +0530

    Create leet_28.py

---
+++ leet_28.py
@@ -0,0 +1,17 @@
+# Find the Index of the First Occurence in String
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        b = 0
+        if needle in haystack:
+            for i in range(len(haystack)):
+                if haystack[i] == needle[0]:
+                    for j in range(i, len(haystack)):
+                        name = haystack[i:j+1]  # Simplify the substring extraction
+                        if name == needle:
+                            b = i
+                            return b
+            return b
+        else:
+            return -1
+
