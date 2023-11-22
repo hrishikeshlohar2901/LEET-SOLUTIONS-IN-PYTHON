87b8717911c6d9988a6a7d7972838ce1aa8adf10
Hrishikesh Keraba Lohar <117097875+hrishikeshlohar2901@users.noreply.github.com>
2023-11-22 23:01:00 +0530

    Create leet_169.py

---
+++ leet_169.py
@@ -0,0 +1,13 @@
+# Majority Element
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        n = len(nums)//2
+        nums.sort()
+        i=0
+        while i<len(nums):
+            if nums.count(nums[i])>n:
+                return nums[i] 
+                i=i+nums.count(nums[i])
+            else:
+                i=i+nums.count(nums[i])
