72baee6b9499e14fd87f9873c5475bb1bc30d090
Hrishikesh Keraba Lohar <117097875+hrishikeshlohar2901@users.noreply.github.com>
2023-11-22 22:17:01 +0530

    Create leet_1.py

---
+++ leet_1.py
@@ -0,0 +1,15 @@
+#two sum
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        flag = False
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j] == target:
+                    flag = True
+                    #print('Result = {} and {}'.format(nums[i],nums[j]))  
+                    return [i,j]           
+        if flag == False:
+            print('The number does not exist')
+
+        
