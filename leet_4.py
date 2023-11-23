d168ef36456eba19eb1e9511e81328e4fc9cb3d1
Hrishikesh Keraba Lohar <117097875+hrishikeshlohar2901@users.noreply.github.com>
2023-11-23 11:10:20 +0530

    Update leet_4.py

--- leet_4.py
+++ leet_4.py
@@ -1,5 +1,5 @@
 # median of two soted arrays 
-
+# https://leetcode.com/problems/median-of-two-sorted-arrays/
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         n3 = nums1 + nums2
