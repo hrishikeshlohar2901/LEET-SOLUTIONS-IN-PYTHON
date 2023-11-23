6ecb488476655bced8e93187a1bc909868c6de0f
Hrishikesh Keraba Lohar <117097875+hrishikeshlohar2901@users.noreply.github.com>
2023-11-23 10:43:47 +0530

    Update leet_1523.py

--- leet_1523.py
+++ leet_1523.py
@@ -1,5 +1,5 @@
 #  Count Odd Numbers in an Interval Range
-
+# https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/
 class Solution:
     def countOdds(self, low: int, high: int) -> int:
         a=(high-low)+1
