73fc79e8505cc55512612e510acbb0aaaba804c7
Hrishikesh Keraba Lohar <117097875+hrishikeshlohar2901@users.noreply.github.com>
2023-11-26 23:08:20 +0530

    Create leet_43.py

---
+++ leet_43.py
@@ -0,0 +1,8 @@
+#multiplying 2 strings
+
+#https://leetcode.com/problems/multiply-strings/submissions/
+
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        return (str(int(num1)*int(num2)))
+        
